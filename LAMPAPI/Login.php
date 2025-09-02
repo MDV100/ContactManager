7b47f872728e5961f1a3f976a3caf16b6466467b
MDV100 <michaeldvogt01@gmail.com>
2025-09-02 03:28:43 +0000

    initial Colors Lab code

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,64 @@
+<?php
+    // This header allows all domains to access your script.
+    // It is a security risk in production, so replace "*" with your frontend domain.
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: PUT, GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    // Set the content type header to JSON
+    header("Content-Type: application/json; charset=UTF-8");
+
+    $inData = getRequestInfo();
+
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
+
+    $conn = new mysqli("localhost", "vogt", "password", "COP4331");
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if( $row = $result->fetch_assoc() )
+        {
+            returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+        }
+        else
+        {
+            returnWithError("No Records Found");
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        // Headers are now set outside this function
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo( $firstName, $lastName, $id )
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+?>

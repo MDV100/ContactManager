ea06111415315998531fac9f6f473460803e76d2
Ehsan Ayoubi <ehsanayoubi2005@gmail.com>
2025-09-22 19:52:05 -0400

    fix: search contacts

--- LAMPAPI/searchContacts.php
+++ LAMPAPI/searchContacts.php
@@ -14,7 +14,7 @@
 	{
 		$stmt = $conn->prepare("SELECT ID, FirstName, LastName, Phone, Email FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR CONCAT(FirstName, ' ', LastName) LIKE ? OR Phone LIKE ? OR Email LIKE ?) AND UserID=?");
 		$searchName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ssssss", $searchName, $searchName, $searchName, $searchName, $inData["userId"]);
+		$stmt->bind_param("ssssss", $searchName, $searchName, $searchName, $searchName, $searchName, $inData["userId"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
